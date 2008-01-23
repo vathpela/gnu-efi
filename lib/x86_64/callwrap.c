f3859c89316b4988245a3f46554b83e49160582a
noxorc <noxorc@8f1d87b7-2c27-0410-8acf-878dfd288f87>
2008-01-23 14:07:43 +0000

    fix to clobbering regs, from Huang Ying
    git-svn-id: https://gnu-efi.svn.sourceforge.net/svnroot/gnu-efi/trunk/gnu-efi-3.0@44 8f1d87b7-2c27-0410-8acf-878dfd288f87

--- lib/x86_64/callwrap.c
+++ lib/x86_64/callwrap.c
@@ -1,122 +1,106 @@
-/*++
+/*
+ * Convert SysV calling convention to EFI x86_64 calling convention
+ *
+ *  Copyright (C) 2007-2010 Intel Corp
+ *	Bibo Mao <bibo.mao@intel.com>
+ *	Chandramouli Narayanan<mouli@linux.intel.com>
+ *	Huang Ying <ying.huang@intel.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ */
 
-Copyright (c) 2006  Intel Corporation
-
-Module Name:
-
-    hw.c
-
-Abstract:
-
-    Debug library functions for Hardware IO access
-
-
-
-Revision History
-
---*/
 #include "efi.h"
 #include "efistdarg.h"
 
-#define EFI_ARG_NUM_MAX 10
-#define EFI_REG_ARG_NUM 4
+UINT64 efi_call0(void *func);
+UINT64 efi_call1(void *func, UINT64 arg1);
+UINT64 efi_call2(void *func, UINT64 arg1, UINT64 arg2);
+UINT64 efi_call3(void *func, UINT64 arg1, UINT64 arg2, UINT64 arg3);
+UINT64 efi_call4(void *func, UINT64 arg1, UINT64 arg2, UINT64 arg3,
+		 UINT64 arg4);
+UINT64 efi_call5(void *func, UINT64 arg1, UINT64 arg2, UINT64 arg3,
+		 UINT64 arg4, UINT64 arg5);
+UINT64 efi_call6(void *func, UINT64 arg1, UINT64 arg2, UINT64 arg3,
+		 UINT64 arg4, UINT64 arg5, UINT64 arg6);
+UINT64 efi_call7(void *func, UINT64 arg1, UINT64 arg2, UINT64 arg3,
+		 UINT64 arg4, UINT64 arg5, UINT64 arg6, UINT64 arg7);
+UINT64 efi_call8(void *func, UINT64 arg1, UINT64 arg2, UINT64 arg3,
+		 UINT64 arg4, UINT64 arg5, UINT64 arg6, UINT64 arg7,
+		 UINT64 arg8);
+UINT64 efi_call9(void *func, UINT64 arg1, UINT64 arg2, UINT64 arg3,
+		 UINT64 arg4, UINT64 arg5, UINT64 arg6, UINT64 arg7,
+		 UINT64 arg8, UINT64 arg9);
+UINT64 efi_call10(void *func, UINT64 arg1, UINT64 arg2, UINT64 arg3,
+		 UINT64 arg4, UINT64 arg5, UINT64 arg6, UINT64 arg7,
+		 UINT64 arg8, UINT64 arg9, UINT64 arg10);
 
-/* Convert SysV calling convention to EFI x86_64 calling convention */
+#define EFI_ARG_NUM_MAX 10
 
 EFI_STATUS uefi_call_wrapper(void *fp, unsigned long va_num, ...)
 {
 	va_list ap;
 	int i;
 	unsigned long args[EFI_ARG_NUM_MAX];
-	unsigned int arg_size,stack_adjust_size;
-	EFI_STATUS status;
 
-	if (va_num > EFI_ARG_NUM_MAX || va_num<0)	{
+	if (va_num > EFI_ARG_NUM_MAX || va_num < 0) {
 		return EFI_LOAD_ERROR;
 	}
-	if (va_num==0)
-		/* There is no need to convert arguments for void argument. */
-		__asm__ __volatile__("call *%0;ret;"::"r"(fp));
-
-	/* The EFI arguments is stored in an array. Then later on it will be 
-	 * pushed into stack or passed to registers according to MS ABI.
-	 */
 	va_start(ap, va_num);
 	for (i = 0; i < va_num; i++) {
-		args[i] = va_arg(ap, unsigned long);
+		args[i] = va_arg(ap, UINT64);
 	}
 	va_end(ap);
-	arg_size = va_num*8;
-	stack_adjust_size = (va_num > EFI_REG_ARG_NUM? EFI_REG_ARG_NUM : va_num)*8;
-
-	/* Starting from here, assembly code makes sure all registers used are
-	 * under controlled by our code itself instead of by gcc.
-	 */
-	/* Start converting SysV calling convention to MS calling convention. */
-	__asm__ __volatile__(
-		/* 0. Save preserved registers. EFI call may clobbered them. */
-		"		pushq %%rbp;pushq %%rbx;pushq %%r12;"
-		"		pushq %%r13;pushq %%r14;pushq %%r15;"
-		/* 1. Push arguments passed by stack into stack. */
-		"		mov %1, %%r12;"
-		"		mov %3, %%r13;"
-		"		mov %1, %%rax;"
-		"		dec %%rax;"
-		"		mov $8, %%bl;"
-		"		mul %%bl;"
-		"		add %%rax, %%r13;"
-		"lstack:"
-		"		cmp $4, %%r12;"
-		"		jle lregister;" 
-		"		pushq (%%r13);"
-		"		sub $8, %%r13;"
-		"		dec %%r12;" 
-		"		jmp lstack;"
-		/* 2. Move arguments passed by registers into registers.
-		 *    rdi->rcx, rsi->rdx, rdx->r8, rcx->r9.
-		 */
-		"lregister:"	
-		"		mov %3, %%r14;"
-		"		mov $0, %%r12;"
-		"lloadregister:"
-		"		cmp %1, %%r12;"
-		"		jge lcall;"
-		"		mov (%%r14), %%rcx;"
-		"		inc %%r12;"
-		"		cmp %1, %%r12;"
-		"		jge lcall;"
-		"		mov 8(%%r14), %%rdx;"
-		"		inc %%r12;"
-		"		cmp %1, %%r12;"
-		"		jge lcall;"
-		"		mov 0x10(%%r14), %%r8;"
-		"		inc %%r12;"
-		"		cmp %1, %%r12;"
-		"		jge lcall;"
-		"		mov 0x18(%%r14), %%r9;"
-		/* 3. Save stack space for those register arguments. */
-		"lcall:				"
-		"		sub %2, %%rsp;"
-		/* 4. Save arg_size to r12 which is preserved in EFI call. */
-		"		mov %4, %%r12;"
-		/* 5. Call EFI function. */
-		"		call *%5;"
-		/* This code was not there before */
-		"		mov %%rax, %0;"
-		/* 6. Restore stack space reserved for those register
-		 * arguments.
-		 */
-		"		add %%r12, %%rsp;"
-		/* 7. Restore preserved registers. */
-		"		popq %%r15;popq %%r14;popq %%r13;"
-		"		popq %%r12;popq %%rbx;popq %%rbp;"
-		:"=r"(status)
-		:"r"((unsigned long)va_num),
-		 "r"((unsigned long)stack_adjust_size),
-		 "r"(args),
-		 "r"((unsigned long)arg_size),
-		 "r"(fp)
-		:"rsp","rbx","rax","r11","r12","r13","r14","rcx","rdx","r8","r9" 
-		);
-	return status;
+	/* As the number of args grows extend it appropriately */
+	switch (va_num) {
+	case 0:
+		return efi_call0(fp);
+	case 1:
+		return efi_call1(fp, args[0]);
+	case 2:
+		return efi_call2(fp,
+				 args[0], args[1]);
+	case 3:
+		return efi_call3(fp,
+				 args[0], args[1], args[2]);
+	case 4:
+		return efi_call4(fp,
+				 args[0], args[1], args[2], args[3]);
+	case 5:
+		return efi_call5(fp,
+				 args[0], args[1], args[2], args[3],
+				 args[4]);
+	case 6:
+		return efi_call6(fp,
+				 args[0], args[1], args[2], args[3],
+				 args[4], args[5]);
+	case 7:
+		return efi_call7(fp,
+				 args[0], args[1], args[2], args[3],
+				 args[4], args[5], args[6]);
+	case 8:
+		return efi_call8(fp,
+				 args[0], args[1], args[2], args[3],
+				 args[4], args[5], args[6], args[7]);
+	case 9:
+		return efi_call9(fp,
+				 args[0], args[1], args[2], args[3],
+				 args[4], args[5], args[6], args[7],
+				 args[8]);
+	case 10:
+		return efi_call10(fp,
+				  args[0], args[1], args[2], args[3],
+				  args[4], args[5], args[6], args[7],
+				  args[8], args[9]);
+	default:
+		return EFI_LOAD_ERROR;
+	}
 }
