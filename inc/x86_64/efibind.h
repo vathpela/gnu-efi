23cb67194165be6b3ee103bd09eee965f174187e
noxorc <noxorc@8f1d87b7-2c27-0410-8acf-878dfd288f87>
2011-12-12 16:04:11 +0000

    3.0p - fixes redefined types compilation failure x86_64.
    git-svn-id: https://gnu-efi.svn.sourceforge.net/svnroot/gnu-efi/trunk/gnu-efi-3.0@76 8f1d87b7-2c27-0410-8acf-878dfd288f87

--- inc/x86_64/efibind.h
+++ inc/x86_64/efibind.h
@@ -16,7 +16,8 @@ Abstract:
 Revision History
 
 --*/
-
+#ifndef X86_64_EFI_BIND
+#define X86_64_EFI_BIND
 #ifndef __GNUC__
 #pragma pack()
 #endif
@@ -278,5 +279,7 @@ UINTN uefi_call_wrapper(void *func, unsigned long va_num, ...);
 
 #ifdef _MSC_EXTENSIONS
 #pragma warning ( disable : 4731 )  // Suppress warnings about modification of EBP
+#endif
+
 #endif
 
