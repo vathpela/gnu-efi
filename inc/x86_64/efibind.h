787cb7fcd8954f8865249128d99626e4d2c0c252
Nigel Croxon <ncroxon@redhat.com>
2017-02-17 16:24:26 -0500

    fix clang support.
    Tested:
      make all apps + tests apps/ on x86_64
      make CC=clang all apps + tests apps/ on x86_64
    
    Signed-off-by: David Decotigny <ddecotig@gmail.com>
    Signed-off-by: Nigel Croxon <ncroxon@redhat.com>

--- inc/x86_64/efibind.h
+++ inc/x86_64/efibind.h
@@ -25,6 +25,8 @@ Revision History
 #if defined(GNU_EFI_USE_MS_ABI)
     #if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 7))
         #define HAVE_USE_MS_ABI 1
+    #elif defined(__clang__)
+        #define HAVE_USE_MS_ABI 1
     #else
         #error Compiler is too old for GNU_EFI_USE_MS_ABI
     #endif
