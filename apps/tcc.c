9d0d9415916e581991dea952ee1252389400a3fc
Nigel Croxon <ncroxon@redhat.com>
2017-03-10 16:02:03 -0500

    apps/tcc: tcc marcked ok for non x86-64 archs
    Tested on qemu (x86-64 and arm64).
    
    Signed-off-by: David Decotigny <ddecotig@gmail.com>
    Signed-off-by: Nigel Croxon <ncroxon@redhat.com>

--- apps/tcc.c
+++ apps/tcc.c
@@ -324,11 +324,7 @@ efi_main (EFI_HANDLE *image, EFI_SYSTEM_TABLE *systab)
 	InitializeLib(image, systab);
 	PoolAllocationType = 2; /* klooj */
 
-#ifndef __x86_64__
-	uefi_call_wrapper(systab->ConOut->OutputString, 2, systab->ConOut,
-		L"This test is only valid on x86_64\n");
-	return EFI_UNSUPPORTED;
-#else
+#ifdef __x86_64__
 	__asm__ volatile("out %0,%1" : : "a" ((uint8_t)0x14), "dN" (0x80));
 #endif
 
