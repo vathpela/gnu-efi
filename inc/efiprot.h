b499899c9cb7abf5be7bcf23cdd55aeaf857bf72
Peter Jones <pjones@redhat.com>
2020-05-04 18:12:11 -0400

    Add EFI_LOADED_IMAGE_DEVICE_PATH_PROTOCOL_GUID.

--- inc/efiprot.h
+++ inc/efiprot.h
@@ -1243,7 +1243,8 @@ typedef struct {
 
 typedef EFI_LOADED_IMAGE_PROTOCOL EFI_LOADED_IMAGE;
 
-
+#define EFI_LOADED_IMAGE_DEVICE_PATH_PROTOCOL_GUID \
+    {0xbc62157e, 0x3e33, 0x4fec, {0x99, 0x20, 0x2d, 0x3b, 0x36, 0xd7, 0x50, 0xdf} }
 
 /*
  * Random Number Generator Protocol
