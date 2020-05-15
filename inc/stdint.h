4f08b34bcd11d006ac21bba343c02161afb3ae80
Peter Jones <pjones@redhat.com>
2020-05-15 15:05:59 -0400

    TEMPORARY Define a bunch of _MAX stuff in stdint.h

--- inc/stdint.h
+++ inc/stdint.h
@@ -12,4 +12,11 @@ typedef signed char int8_t;
 
 typedef signed long intptr_t;
 
+#define SIZE_MAX 0xffffffffffffffffull
+#define SSIZE_MAX 0x7fffffffffffffffull
+#define UINT32_MAX 0xfffffffful
+#define INT32_MAX 0x7ffffffful
+#define UINT64_MAX 0xffffffffffffull
+#define INT64_MAX 0x7ffffffffffffull
+
 #endif
