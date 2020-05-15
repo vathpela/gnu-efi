f9a2c177c66626f508e2d94411b62db6a63ae868
Peter Jones <pjones@redhat.com>
2020-05-15 14:53:14 -0400

    temporary - add a stdint.h for some reason

---
+++ inc/stdint.h
@@ -0,0 +1,15 @@
+#ifndef PJSTDINT_H
+#define PJSTDINT_H
+
+typedef unsigned long uint64_t;
+typedef long int64_t;
+typedef unsigned int uint32_t;
+typedef int int32_t;
+typedef unsigned short uint16_t;
+typedef short int16_t;
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+
+typedef signed long intptr_t;
+
+#endif
