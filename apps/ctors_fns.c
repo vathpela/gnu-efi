6f333cfc914e7cfacc4d3ef3e034e4483f0ba8b2
Callum Farmer <gmbr3@opensuse.org>
2023-06-21 20:27:43 +0100

    Tail calls break ctors/dtors
    Add macro EFI_NO_TAIL_CALL which disables tail call
    optimization on a per-function basis
    
    Signed-off-by: Callum Farmer <gmbr3@opensuse.org>

--- apps/ctors_fns.c
+++ apps/ctors_fns.c
@@ -9,14 +9,14 @@
 
 int constructed_value = 0;
 
-static void __attribute__((__constructor__)) ctor(void)
+static void __attribute__((__constructor__)) EFI_NO_TAIL_CALL ctor(void)
 {
 	Print(L"%a:%d:%a() constructed_value:%d\n", __FILE__, __LINE__, __func__, constructed_value);
 	constructed_value = 1;
 	Print(L"%a:%d:%a() constructed_value:%d\n", __FILE__, __LINE__, __func__, constructed_value);
 }
 
-static void __attribute__((__destructor__)) dtor(void)
+static void __attribute__((__destructor__)) EFI_NO_TAIL_CALL dtor(void)
 {
 	Print(L"%a:%d:%a() constructed_value:%d\n", __FILE__, __LINE__, __func__, constructed_value);
 	constructed_value = 0;
--- inc/eficompiler.h
+++ inc/eficompiler.h
@@ -19,6 +19,16 @@ Abstract:
 #endif
 
 #ifdef _MSC_EXTENSIONS
+#define EFI_NO_TAIL_CALL
+#else
+#ifdef __clang__
+#define EFI_NO_TAIL_CALL __attribute__((disable_tail_calls))
+#else
+#define EFI_NO_TAIL_CALL __attribute__((optimize("no-optimize-sibling-calls")))
+#endif
+#endif
+
+#ifdef _MSC_EXTENSIONS
 #define ALIGN(x) __declspec(align(x))
 #else
 #define ALIGN(x) __attribute__((__aligned__(x)))
